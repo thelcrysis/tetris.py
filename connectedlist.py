da51bb4419d8acf8506bcbc1001c51e2122ec120
thelcrysis <thelcrysis@gmail.com>
2019-11-27 16:14:56 +0100

    algorithm for any number ofconnected lists

--- connectedlist.py
+++ connectedlist.py
@@ -8,7 +8,12 @@ class connectedList:
                 raise TypeError("Second param must consist of numbers")
         self.listA = listA.copy()
         self.listB = listB.copy()
-
+    @staticmethod
+    def testdefine():
+        a = [40,60,70,30,20,50,10]
+        b = ['d','f','g','c','b','e','a']
+        c = [4,6,7,3,2,5,1]
+        return a,b,c
     def printLists(self):
         print(self.listA)
         print(self.listB)
@@ -28,3 +33,32 @@ class connectedList:
             self.listA.append(consListA[index])
             consListA.pop(index)
             consListB.pop(index)
+    @staticmethod
+    def sortMulConnectedLists(*args):
+        #checking if every param is of right type
+        args = list(args)
+        for arg in args:
+            if type(arg) != list:
+                raise TypeError("params must be lists")
+        sortingListIndex = len(args)-1
+        sortingList = args.pop(sortingListIndex)
+        for value in sortingList:
+            if type(value) != int and type(value) != float:
+                raise TypeError("Second param must consist of numbers")
+
+        consLists = []
+        resultLists = []
+        for arg in args:
+            resultLists.append([])
+            consLists.append(arg)
+        consSortingList = sortingList.copy()
+        sortingList.sort()
+        
+        for i in sortingList:
+            index = consSortingList.index(i)
+            for j in range(len(resultLists)):
+                resultLists[j].append(consLists[j][index])
+                consLists[j].pop(index)
+            consSortingList.pop(index)
+        resultLists.append(sortingList)
+        return resultLists        
